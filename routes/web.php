a580a1bb607dcee76aece0e911c0097156935b2c
TENMAJKL <tenmajkl@liskyjidlokod.fun>
2023-09-04 20:29:56 +0200

    makes update post

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use App\Controllers\Page;
 use App\Controllers\Update;
 use Lemon\Route;
 
-Route::get('/update', [Update::class, 'handle']);
+Route::post('/update', [Update::class, 'handle']);
 Route::get('/{page}', [Page::class, 'handle']);
 Route::get('/', [Page::class, 'first']);
